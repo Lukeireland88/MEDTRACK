bab8fb1da01fa7121ef287a577706b5600a6b059
Lukeireland88 <luke.ireland@outlook.com>
2026-09-16 13:58:46 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -50,28 +50,16 @@ export default defineConfig({
         background_color: '#eef4ff',
         icons: [
           {
-            src: '/icon-192.png',
+            src: 'icon-192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any',
           },
           {
-            src: '/icon-512.png',
+            src: 'icon-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any',
-          },
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
           },
         ],
       },
